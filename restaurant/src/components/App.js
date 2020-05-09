8f09c2814b5f4e5d33307c5b38b54347ec8d4008
Markese Straughter <Kese08@hotmail.com>
2020-05-09 13:06:14 -0700

    pulling

--- restaurant/src/components/App.js
+++ restaurant/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/App.css';
+import '../styles/Cards.css';
 import { Provider } from 'react-redux'
 import store from '../redux/store'
 import Example from './Example'
+import Cards from './Cards'
+
 export default () => <div>
   <Provider store={store}>
-    <Example />
+    <Cards />
   </Provider>
 </div>
---
+++ restaurant/src/components/Cards.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+function Cards (){
+
+
+
+return(
+
+
+    <div >
+        <div>
+            <h1>adasdas</h1>
+            <span>asdasd</span>
+            <span>asdasdsdfdf</span>
+        </div>
+
+    </div>
+)
+
+
+}
+
+
+
+export default Cards
