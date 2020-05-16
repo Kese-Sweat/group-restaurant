6a8cbd4c552fda169a3f8c79f7339ff756bd4b70
Markese Straughter <Kese08@hotmail.com>
2020-05-16 11:11:04 -0700

    added photo and paragraph

--- restaurant/src/components/Landing.js
+++ restaurant/src/components/Landing.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Cards from './Cards'
 import Menu from './Menu'
+import Story from './StoryContent'
 import '../styles/Cards.css';
+import StoryContent from './StoryContent';
+
 
 export default () => {
   return (
     <div>
       <Cards/>
       <Menu/>
+      <StoryContent/>
     </div>
   )
 }
