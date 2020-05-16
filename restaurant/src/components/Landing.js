accbccc986f6b4059902d906ea3665ba782cf9c6
Markese Straughter <Kese08@hotmail.com>
2020-05-16 10:23:44 -0700

    added Menu

--- restaurant/src/components/Landing.js
+++ restaurant/src/components/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Cards from './Cards'
+import Menu from './Menu'
 import '../styles/Cards.css';
 
 export default () => {
   return (
     <div>
       <Cards/>
+      <Menu/>
     </div>
   )
 }
