8bcf5c94027fd870e71b5d16518799ec27972a21
Tyler French <tylerfrench89@gmail.com>
2020-05-16 12:40:47 -0700

    bottom bar for trademark

---
+++ restaurant/src/components/BottomBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/BottomBar.css';
+
+export default () => {
+    return (
+        <div className="bottomBar">
+            <div className="info">{'\u00A9'} 2010 Restaurant name.</div>
+        </div>
+    )
+   
+}
